4cd7ed550df7e00a78524b3e542b25fd010ff6f1
Tommy <tommy_tom2000@hotmail.com>
2023-06-12 17:43:21 +1000

    begin moving to Optee. Not tested

--- board/stm32mp151_dev_board/post-image.sh
+++ board/stm32mp151_dev_board/post-image.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 
-#
-# atf_image extracts the ATF binary image from DTB_FILE_NAME that appears in
-# BR2_TARGET_ARM_TRUSTED_FIRMWARE_ADDITIONAL_VARIABLES in ${BR_CONFIG},
-# then prints the corresponding file name for the genimage
-# configuration file
-#
-atf_image()
-{
-	#
-	# TODO: Make the FIP image here
-	#
-	local ATF_VARIABLES="$(sed -n 's/^BR2_TARGET_ARM_TRUSTED_FIRMWARE_ADDITIONAL_VARIABLES="\([\/a-zA-Z0-9_=. \-]*\)"$/\1/p' ${BR2_CONFIG})"
-
-	if grep -Eq "DTB_FILE_NAME=stm32mp151_dev_board-tf-a.dtb" <<< ${ATF_VARIABLES}; then
-        echo "tf-a-stm32mp151_dev_board-tf-a.stm32"
-	fi
-}
-
 main()
 {
-	local ATFBIN="$(atf_image)"
+	local ATFBIN="tf-a-stm32mp151_dev_board-tf-a.stm32"
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
